06c01f2b09f5a6681c3818ebe7edf20200216488
Jim Hopkinson <jahopki@hotmail.com>
2020-06-29 11:50:56 -0600

    commit inquirer

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+  .then(answers => console.log(answers));
+  
